88490387a918fdc76b08911e064c592b41b57fee
AnlakHui <33713998+AnlakHui@users.noreply.github.com>
2022-05-04 02:56:05 +0800

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -34,4 +34,4 @@ echo "以PUID=${PUID}，PGID=${PGID}的身份启动程序..."
 echo "注意：日志将停止打印，请通过文件或WEB页面查看日志"
 #chown -R ${PUID}:${PGID} /config ${WORKDIR} /var/log/supervisor/
 umask ${UMASK}
-exec su-exec ${PUID}:${PGID} /msg2bark/run.py
+sh /msg2bark/run.py
